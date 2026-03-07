a41f3147b6d02b68d61bbe0c87be11d44a882c08
a19albertons <a19albertons@iessanclemente.net>
2026-03-07 19:14:44 +0100

    Refactorización primer fragmento

--- Fase 2/app/src/main/java/com/example/tustareas/fragmentos/InicioFragment.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/fragmentos/InicioFragment.kt
@@ -1,11 +1,11 @@
-package com.example.tustareas
+package com.example.tustareas.fragmentos
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import com.example.tustareas.R
 
 class InicioFragment : Fragment() {
 
