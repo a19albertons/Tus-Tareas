e7cb2af8671c5f99846f3c9ffd598baf6ff7a30b
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-03-10 22:51:00 +0100

    Añadir al dto la relation de tarea

--- Fase 2/app/src/main/java/com/example/tustareas/dto/ProyectoDTO.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/dto/ProyectoDTO.kt
@@ -6,18 +6,23 @@ import androidx.room.Relation
 import com.example.tustareas.modelos.Etiqueta
 import com.example.tustareas.modelos.Proyecto
 import com.example.tustareas.modelos.ProyectoEtiqueta
+import com.example.tustareas.modelos.Tarea
 
 /**
- * Clase que representa el Data Transfer Object entre Proyecto y la lista de tareas
+ * Clase que representa el Data Transfer Object entre Proyecto y la lista de tareas, además de Proyecto con sus etiquetas
  */
 data class ProyectoDTO (
     @Embedded var proyecto: Proyecto,
     @Relation(
         parentColumn = "idProyecto",
         entityColumn = "idEtiqueta",
-        associateBy = Junction(ProyectoEtiqueta::class)
+        associateBy = Junction(ProyectoEtiqueta::class,)
     )
-    var etiquetas: List<Etiqueta>
-
+    var etiquetas: List<Etiqueta>,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id"
+    )
+    var tareas: List<Tarea>
 ) {
 }
