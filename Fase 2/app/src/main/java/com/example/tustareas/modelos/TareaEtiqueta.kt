b4db66e449265a2ff7e32b206555c3525e18f12c
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-03-10 21:58:17 +0100

    Correción nombre de clase

--- Fase 2/app/src/main/java/com/example/tustareas/modelos/TareaEtiqueta.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/modelos/TareaEtiqueta.kt
@@ -3,7 +3,7 @@ package com.example.tustareas.modelos
 import androidx.room.Entity
 
 @Entity(tableName = "TareaEtiquetas", primaryKeys = ["idTarea", "idEtiqueta"])
-data class TareaEtiquetas(
+data class TareaEtiqueta(
     var idTarea: Int,
     var idEtiqueta: Int
 ) {
