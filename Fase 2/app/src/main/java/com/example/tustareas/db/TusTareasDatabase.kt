6b1fe7216394272f789397801bcd700465f1bb25
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-03-11 21:47:06 +0100

    Aplicación que crea la bd en la aplicacion

---
+++ Fase 2/app/src/main/java/com/example/tustareas/db/TusTareasDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.tustareas.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.tustareas.modelos.Convertidor
+import com.example.tustareas.modelos.Etiqueta
+import com.example.tustareas.modelos.Proyecto
+import com.example.tustareas.modelos.ProyectoEtiqueta
+import com.example.tustareas.modelos.Tarea
+import com.example.tustareas.modelos.TareaEtiqueta
+
+/**
+ * Base de datos de tus tareas
+ */
+@Database(entities = [Proyecto::class, Tarea::class, Etiqueta::class, ProyectoEtiqueta::class, TareaEtiqueta::class], version = 1, exportSchema = false)
+@TypeConverters(Convertidor::class)
+abstract class TusTareasDatabase : RoomDatabase() {
+    companion object {
+        @Volatile
+        private var INSTANCE: TusTareasDatabase? = null
+
+        fun getDatabase(context: Context): TusTareasDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder<TusTareasDatabase>(
+                    context.applicationContext,
+                    TusTareasDatabase::class.java,
+                    "baseDatos.db"
+                ).createFromAsset("baseDatos.db").build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
+}
