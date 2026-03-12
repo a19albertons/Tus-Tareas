f9bd3dfd96dab402c21a62319ca8ce0048aa818f
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-03-12 17:31:54 +0100

    Definición basica del view model

---
+++ Fase 2/app/src/main/java/com/example/tustareas/modelView/TusTareasModel.kt
@@ -0,0 +1,17 @@
+package com.example.tustareas.modelView
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import com.example.tustareas.db.TusTareasDatabase
+import com.example.tustareas.repository.TusTareasRepository
+import java.util.Date
+
+/**
+ * ViewModel que une la aplicacion con la base de datos
+ */
+class TusTareasModel(application: Application): AndroidViewModel(application) {
+    private val repository = TusTareasRepository(TusTareasDatabase.getDatabase( application))
+
+    fun obtenerTareasTerminanDiaEspecifico(fecha: Date) = repository.obtenerTareasTerminanDiaEspecifico(fecha)
+}
