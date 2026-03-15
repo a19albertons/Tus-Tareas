1f437da2c5caca40b8980f44076f1034e024ed77
a19albertons <a19albertons@iessanclemente.net>
2026-03-15 20:41:40 +0100

    Soporte para borrado en cascada en las tablas intermedias (N:N)

--- Fase 2/app/src/main/java/com/example/tustareas/modelos/ProyectoEtiqueta.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/modelos/ProyectoEtiqueta.kt
@@ -1,11 +1,29 @@
 package com.example.tustareas.modelos
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
 /**
  * Clase que representa la relación N:N entre Proyecto y Etiquetas
  */
-@Entity(tableName = "ProyectoEtiquetas", primaryKeys = ["idProyecto", "idEtiqueta"])
+@Entity(
+    tableName = "ProyectoEtiquetas",
+    primaryKeys = ["idProyecto", "idEtiqueta"],
+    foreignKeys = [
+        ForeignKey(
+            entity = Etiqueta::class,
+            parentColumns = ["id"],
+            childColumns = ["idEtiqueta"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Proyecto::class,
+            parentColumns = ["id"],
+            childColumns = ["idProyecto"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class ProyectoEtiqueta(
     var idProyecto: Int,
     var idEtiqueta: Int
--- Fase 2/app/src/main/java/com/example/tustareas/modelos/TareaEtiqueta.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/modelos/TareaEtiqueta.kt
@@ -1,11 +1,29 @@
 package com.example.tustareas.modelos
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
 /**
  * Clase que representa la relación N:N entre Tarea y Etiquetas
  */
-@Entity(tableName = "TareaEtiquetas", primaryKeys = ["idTarea", "idEtiqueta"])
+@Entity(
+    tableName = "TareaEtiquetas",
+    primaryKeys = ["idTarea", "idEtiqueta"],
+    foreignKeys = [
+        ForeignKey(
+            entity = Etiqueta::class,
+            parentColumns = ["id"],
+            childColumns = ["idEtiqueta"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Tarea::class,
+            parentColumns = ["id"],
+            childColumns = ["idTarea"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class TareaEtiqueta(
     var idTarea: Int,
     var idEtiqueta: Int
