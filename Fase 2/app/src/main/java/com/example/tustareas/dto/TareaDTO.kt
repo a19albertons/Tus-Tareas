2fab0116e555120f12ad5d542fd70b7da54118e8
a19albertons <a19albertons@iessanclemente.net>
2026-03-19 23:00:23 +0100

    TareaDTO soporta pacelize

--- Fase 2/app/src/main/java/com/example/tustareas/dto/TareaDTO.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/dto/TareaDTO.kt
@@ -1,15 +1,18 @@
 package com.example.tustareas.dto
 
+import android.os.Parcelable
 import androidx.room.Embedded
 import androidx.room.Junction
 import androidx.room.Relation
 import com.example.tustareas.modelos.Etiqueta
 import com.example.tustareas.modelos.Tarea
 import com.example.tustareas.modelos.TareaEtiqueta
+import kotlinx.parcelize.Parcelize
 
 /**
  * Clase que representa el Data Transfer Object entre Tarea y la lista de etiquetas
  */
+@Parcelize
 data class TareaDTO (
     @Embedded var tarea: Tarea,
     @Relation(
@@ -18,6 +21,6 @@ data class TareaDTO (
         associateBy = Junction(TareaEtiqueta::class, parentColumn = "idTarea", entityColumn = "idEtiqueta")
     )
     var etiquetas: List<Etiqueta>
-) {
+) : Parcelable {
 
 }
