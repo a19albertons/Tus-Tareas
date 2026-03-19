ee661677d2adb8605769cededb3a4b77ea92d355
a19albertons <a19albertons@iessanclemente.net>
2026-03-19 11:52:54 +0100

    Nuevo enumerador con las opciones de ordenación

---
+++ Fase 2/app/src/main/java/com/example/tustareas/modelos/OrdenarTareas.kt
@@ -0,0 +1,11 @@
+package com.example.tustareas.modelos
+
+/**
+ * Clase que define las posibilidades de ordenación de tareas
+ */
+enum class OrdenarTareas {
+    FECHA_CREACION_ASC,
+    FECHA_CREACION_DES,
+    FECHA_LIMITE_ASC,
+    FECHA_LIMITE_DES
+}
