9aa097c9879351a3191c36187b22753ece6403c8
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-03-25 19:14:11 +0100

    Creacion adapter ver mas

---
+++ Fase 2/app/src/main/java/com/example/tustareas/adapters/VerMasAdapter.kt
@@ -0,0 +1,106 @@
+package com.example.tustareas.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tustareas.R
+import com.example.tustareas.fragmentos.ListarTareasFragmentDirections
+import com.example.tustareas.fragmentos.VerMasFragment
+import com.example.tustareas.fragmentos.VerMasFragmentDirections
+import com.example.tustareas.modelView.TusTareasModel
+import com.example.tustareas.modelos.Estado
+import com.example.tustareas.modelos.Tarea
+import com.example.tustareas.util.DateHelper
+import com.google.android.material.card.MaterialCardView
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import java.util.Date
+
+/**
+ * Clase que define el adapter de ver más
+ */
+// Modificado a ListAdapter para poder controlar los cambios en el checkbox sin volver al inicio
+class VerMasAdapter(private val tareas: List<Tarea>, private val model: TusTareasModel, private val verMas: Int): ListAdapter<Tarea, VerMasAdapter.VerMasViewHolder>(TareaComprobacionDiferncias()) {
+    // Genera un scope para los procesos secundarios
+    private val scope = MainScope()
+
+    // View holder
+    class VerMasViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nombreTarea: TextView = itemView.findViewById(R.id.nombreTarea)
+        val otroCampo: TextView = itemView.findViewById(R.id.otroCampo)
+        val clickable: MaterialCardView = itemView.findViewById(R.id.clickable)
+        val checkbox: CheckBox = itemView.findViewById(R.id.checkbox)
+    }
+
+    // Inflar el contenido de la vista
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VerMasViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.adapter_ver_mas, parent, false)
+        return VerMasViewHolder(view)
+    }
+
+    // Sobreescritura de valores
+    override fun onBindViewHolder(holder: VerMasViewHolder, posicion: Int) {
+        val objectoActual = getItem(posicion)
+        holder.nombreTarea.text = objectoActual.nombre
+        if (verMas == 1) {
+            holder.otroCampo.text = objectoActual.prioridad.name
+        }
+        else {
+            holder.otroCampo.text = DateHelper.timestampToString(objectoActual.fechaLimite)
+        }
+        holder.clickable.setOnClickListener {
+            it.findNavController().navigate(VerMasFragmentDirections.actionVerMasFragmentToTareaDetallesFragment(objectoActual.id))
+        }
+        // Gestiona el estado de la tarea
+        // Comprueba el estado
+        if (objectoActual.estado == Estado.Completada) {
+            holder.checkbox.isChecked = true
+        }
+        else {
+            holder.checkbox.isChecked = false
+        }
+        // Actualiza el click en consecuencia
+        holder.checkbox.setOnClickListener {
+            if (holder.checkbox.isChecked) {
+                objectoActual.estado = Estado.Completada
+                scope.launch {
+                    model.modificarTarea(objectoActual)
+                }
+            }
+            else {
+                // Control de la fecha EnTiempo o Retrasada
+                if (objectoActual.fechaLimite != null && objectoActual.fechaLimite!! > Date()) {
+                    objectoActual.estado = Estado.EnTiempo
+                }
+                else {
+                    objectoActual.estado = Estado.Retrasada
+                }
+                scope.launch {
+                    model.modificarTarea(objectoActual)
+                }
+            }
+        }
+
+    }
+
+    // Para evitar volver al inicio al hacer scroll al cambiar un checkbox
+    class TareaComprobacionDiferncias : DiffUtil.ItemCallback<Tarea>() {
+        // Comprobar en el global
+        override fun areContentsTheSame(viejaTarea: Tarea, nuevaTarea: Tarea): Boolean {
+            return viejaTarea == nuevaTarea
+        }
+
+
+        // Comprobar algo unico (ids)
+        override fun areItemsTheSame(viejaTarea: Tarea, nuevaTarea: Tarea): Boolean {
+
+            return viejaTarea.id == nuevaTarea.id
+        }
+    }
+}
