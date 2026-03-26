f03d0104f5787492f5c8ed5a56e6d965232e1679
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-03-26 19:59:18 +0100

    Soporte estado para devolver un string para los distintos idiomas

--- Fase 2/app/src/main/java/com/example/tustareas/modelos/Estado.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/modelos/Estado.kt
@@ -1,10 +1,18 @@
 package com.example.tustareas.modelos
 
+import com.example.tustareas.R
+
 /**
  * Clase que representa el enum Estado
  */
 enum class Estado {
     EnTiempo,
     Retrasada,
-    Completada
+    Completada;
+
+    fun labelRes(): Int = when (this) {
+        EnTiempo -> R.string.en_tiempo
+        Retrasada -> R.string.retrasada
+        Completada -> R.string.completada
+    }
 }
