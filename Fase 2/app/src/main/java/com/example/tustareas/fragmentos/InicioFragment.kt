33573bbf33f2b07eee56397c7e2caed1e58a386f
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-03-26 19:02:04 +0100

    Eliminado imports en deshuso en fragmento de inicio

--- Fase 2/app/src/main/java/com/example/tustareas/fragmentos/InicioFragment.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/fragmentos/InicioFragment.kt
@@ -6,17 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.LiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tustareas.R
 import com.example.tustareas.adapters.TareasHoyPendientesAdapter
 import com.example.tustareas.adapters.TareasProximasAdapter
 import com.example.tustareas.adapters.TareasRetrasadasAdapter
-import com.example.tustareas.dao.TareaConsultas
 import com.example.tustareas.databinding.FragmentInicioBinding
 import com.example.tustareas.modelView.TusTareasModel
-import com.example.tustareas.modelos.Tarea
 import com.google.android.material.snackbar.Snackbar
 import java.util.Date
 
