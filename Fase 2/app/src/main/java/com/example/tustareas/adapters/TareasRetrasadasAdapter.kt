1ab1e0e464fa40c1c4be65e38370cb420b617fbd
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-03-26 19:05:13 +0100

    Eliminar imports en deshudo en tareas retrasadas

--- Fase 2/app/src/main/java/com/example/tustareas/adapters/TareasRetrasadasAdapter.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/adapters/TareasRetrasadasAdapter.kt
@@ -3,7 +3,6 @@ package com.example.tustareas.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tustareas.R
