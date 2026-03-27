ff781606df7a6add6c22a0f3daae6974b7f27759
a19albertons <a19albertons@iessanclemente.net>
2026-03-27 22:18:54 +0100

    Eliminar nulos en fragmento ajustes

--- Fase 2/app/src/main/java/com/example/tustareas/fragmentos/AjustesFragment.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/fragmentos/AjustesFragment.kt
@@ -28,7 +28,7 @@ class AjustesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentAjustesBinding.inflate(inflater, container, false)
         val view = binding.root
