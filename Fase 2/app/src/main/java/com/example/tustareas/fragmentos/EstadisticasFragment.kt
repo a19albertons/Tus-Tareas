14dca3ff920431c9e67fccf92d6d7db8a4e07baf
a19albertons <a19albertons@iessanclemente.net>
2026-03-27 21:06:20 +0100

    Correción de nulos en fragmento estadisticas

--- Fase 2/app/src/main/java/com/example/tustareas/fragmentos/EstadisticasFragment.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/fragmentos/EstadisticasFragment.kt
@@ -31,7 +31,7 @@ class EstadisticasFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentEstadisticasBinding.inflate(inflater, container, false)
         val view = binding.root
