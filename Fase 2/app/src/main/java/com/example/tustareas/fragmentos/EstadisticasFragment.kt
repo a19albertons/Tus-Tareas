d3c0db005ddba97ac9f3c2b388a1828c7fbfc4a1
a19albertons <a19albertons@iessanclemente.net>
2026-03-27 21:05:46 +0100

    Removidos imports no utilizados en fragmento estadisticas

--- Fase 2/app/src/main/java/com/example/tustareas/fragmentos/EstadisticasFragment.kt
+++ Fase 2/app/src/main/java/com/example/tustareas/fragmentos/EstadisticasFragment.kt
@@ -2,12 +2,10 @@ package com.example.tustareas.fragmentos
 
 import android.icu.util.Calendar
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import com.example.tustareas.R
 import com.example.tustareas.databinding.FragmentEstadisticasBinding
