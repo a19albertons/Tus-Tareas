cd10a7035215f4cc07cbab45f33aae17dc4bf3c2
a19albertons <a19albertons@iessanclemente.net>
2026-03-29 12:41:20 +0200

    Definición del worker

---
+++ Fase 2/app/src/main/java/com/example/tustareas/workers/ActualizarEstadoWorker.kt
@@ -0,0 +1,33 @@
+package com.example.tustareas.workers
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.tustareas.db.TusTareasDatabase
+import com.example.tustareas.repository.WorkerRepository
+
+/**
+ * Clase que gestiona el paso de tareas que exceden su tiempo limite y aún no estan completadas de acuerdo al usuario
+ */
+class ActualizarEstadoWorker(appContext: Context, workerParams: WorkerParameters): Worker(appContext, workerParams) {
+    override fun doWork(): Result {
+        var devolver = Result.success()
+        try {
+            revisarEstados()
+        }
+        catch (_: Exception) {
+            devolver = Result.failure()
+        }
+
+
+        return devolver
+    }
+
+    // Función que controla la logica de la tarea programada
+    private fun revisarEstados() {
+        val database = TusTareasDatabase.getDatabase(applicationContext)
+        val repositorio = WorkerRepository(database)
+        repositorio.actualizarEstado()
+    }
+}
