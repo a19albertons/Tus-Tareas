078dcce0d178b0b252115e8ddfe323c45f257e95
a19albertons <a19albertons@iessanclemente.net>
2026-04-02 18:23:35 +0200

    Actualizar programación de alarma para que cree notificaciones nueva y envie las no leidas

--- codigo/app/src/main/java/com/example/tustareas/workers/LanzarNotificaciones.kt
+++ codigo/app/src/main/java/com/example/tustareas/workers/LanzarNotificaciones.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.example.tustareas.db.TusTareasDatabase
+import com.example.tustareas.modelos.Notificacion
 import com.example.tustareas.repository.WorkerRepository
 import com.example.tustareas.util.AlarmaHelper
 import kotlinx.coroutines.CoroutineScope
@@ -25,13 +26,28 @@ class LanzarNotificaciones  : BroadcastReceiver() {
                 val db = TusTareasDatabase.getDatabase(contexto!!)
                 val repository = WorkerRepository(db)
 
-                // Lista necesaria
+                // Obtener notificaciones
+                val notificaciones = repository.obtenerTodasLasNotificaciones()
+
+                // Lista tareas retrasadas
                 val tareasRetrasadas = repository.tareasRetrasadasAlarma()
+
+                // Filtramos y comprobamos si la tarea retrasada es nueva
+                for (tarea in tareasRetrasadas) {
+                    if (tarea.id !in notificaciones.map { it.idTarea }) {
+                        repository.anadirNotificacion(Notificacion(0, "Retrasada - ${tarea.nombre}", tarea.descripcion?:"", false, tarea.id))
+                    }
+                }
+
+                // Reenvio de notificaciones no leidas
+                val enviarNotificaciones = repository.enviarNotificaciones()
+
+
 
                 // Vuelve al main para lanzar las tareas
                 withContext(Dispatchers.Main) {
                     // Lanzar notificaciones
-                    AlarmaHelper.invocarAlarma(contexto, tareasRetrasadas)
+                    AlarmaHelper.invocarAlarma(contexto, enviarNotificaciones)
 
                 }
             } finally {
