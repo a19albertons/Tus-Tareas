79343ff99db23902979ed795d21c829ab786071b
a19albertons <a19albertons@iessanclemente.net>
2026-04-04 16:42:54 +0200

    Añadir comentarios consultas de trabajadores

--- codigo/app/src/main/java/com/example/tustareas/dao/WorkerConsultas.kt
+++ codigo/app/src/main/java/com/example/tustareas/dao/WorkerConsultas.kt
@@ -14,6 +14,7 @@ import java.util.Date
  */
 @Dao
 interface WorkerConsultas {
+    // Actualiza todas las tareas retradas de en tiempo a retrasadas cuando corresponde
     @Query("UPDATE tareas SET estado = :estadoNuevo where fechaLimite < :fecha AND estado = :estado")
     fun actualizarEstado(fecha: Date = Date(), estado: Estado = Estado.EnTiempo, estadoNuevo: Estado = Estado.Retrasada)
 
@@ -35,6 +36,7 @@ interface WorkerConsultas {
     @Query("SELECT * FROM notificaciones")
     fun obtenerTodasLasNotificaciones() : List<Notificacion>
 
+    // Añade una notificación a la bd
     @Insert
     fun anadirNotificacion(notificacion: Notificacion)
 
