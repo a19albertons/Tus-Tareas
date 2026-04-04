f06d1cc5905adfcde3ad000b149e7bdd4865a2c8
a19albertons <a19albertons@iessanclemente.net>
2026-04-04 16:33:40 +0200

    Añadir comentarios en listar etiquetas

--- codigo/app/src/main/java/com/example/tustareas/dao/ListarEtiquetasConsultas.kt
+++ codigo/app/src/main/java/com/example/tustareas/dao/ListarEtiquetasConsultas.kt
@@ -5,8 +5,12 @@ import androidx.room.Dao
 import androidx.room.Query
 import com.example.tustareas.modelos.Etiqueta
 
+/**
+ * Clase que representa las consultas en listar etiquetas
+ */
 @Dao
 interface ListarEtiquetasConsultas {
+    // Obtener todas las etiquetas en base al filtro de texto
     @Query("select * from etiquetas where LOWER(nombre) like LOWER('%' || :texto || '%') OR LOWER(descripcion) like LOWER('%' || :texto || '%')")
     fun obtenerEtiquetasFiltradas(texto: String): LiveData<List<Etiqueta>>
 }
