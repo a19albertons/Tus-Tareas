aff832c5a80583d1293b4c61395894317a4ff8e6
a19albertons <a19albertons@iessanclemente.net>
2026-04-04 16:59:41 +0200

    Añadir comentarios en language helper

--- codigo/app/src/main/java/com/example/tustareas/util/LanguageHelper.kt
+++ codigo/app/src/main/java/com/example/tustareas/util/LanguageHelper.kt
@@ -4,7 +4,11 @@ import android.os.LocaleList
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
 
+/**
+ * Clase que ayuda con las funciones especificas de idioma
+ */
 object LanguageHelper {
+    // Obtiene el tag interno de un idioma
     fun etiquetaIdioma(idioma: String): String {
         return when (idioma) {
             "Sistema" -> ""
@@ -14,8 +18,9 @@ object LanguageHelper {
             else -> ""
         }
     }
+    // Aplica el idioma a partir del tag
     fun aplicarIdioma(siglas: String) {
-        val locale = LocaleListCompat.forLanguageTags(siglas ?: "")
+        val locale = LocaleListCompat.forLanguageTags(siglas)
         AppCompatDelegate.setApplicationLocales(locale)
 
     }
