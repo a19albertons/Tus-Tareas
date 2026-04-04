aa40e70a7633fe04b69e67098ea672c19173e44c
a19albertons <a19albertons@iessanclemente.net>
2026-04-04 16:54:00 +0200

    Añadir comentarios en listar etiquetas

--- codigo/app/src/main/java/com/example/tustareas/repository/ListarEtiquetasRepository.kt
+++ codigo/app/src/main/java/com/example/tustareas/repository/ListarEtiquetasRepository.kt
@@ -2,6 +2,9 @@ package com.example.tustareas.repository
 
 import com.example.tustareas.db.TusTareasDatabase
 
+/**
+ * Clase que representa el subrepositorio de listar etiquetas
+ */
 class ListarEtiquetasRepository(database: TusTareasDatabase) {
     private val listarEtiquetasConsultas = database.listarEtiquetasConsultas()
     // Etiquetas filtradas
