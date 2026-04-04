aa130ad9c057280252454ac2db5de2de93d200e1
a19albertons <a19albertons@iessanclemente.net>
2026-04-04 16:55:17 +0200

    Añadir comentarios en repositorios de trabajadores

--- codigo/app/src/main/java/com/example/tustareas/repository/WorkerRepository.kt
+++ codigo/app/src/main/java/com/example/tustareas/repository/WorkerRepository.kt
@@ -9,13 +9,19 @@ import com.example.tustareas.modelos.Notificacion
 class WorkerRepository(database: TusTareasDatabase) {
     private val workerConsultas = database.workerConsultas()
 
+    // Actualzia los estados de las taras
     fun actualizarEstado() = workerConsultas.actualizarEstado()
+
+    // Metodo de la alarma para obtener tareas retrasadas
     suspend fun tareasRetrasadasAlarma() = workerConsultas.tareasRetrasadasAlarma()
 
+    // Recupera todas las notifiaciones
     fun obtenerTodasLasNotificaciones() = workerConsultas.obtenerTodasLasNotificaciones()
 
+    // Añade nuevas notificaciones no creadas de las tareas
     fun anadirNotificacion(notificacion: Notificacion) = workerConsultas.anadirNotificacion(notificacion)
 
+    // Envia las notificaciones no leidas
     suspend fun enviarNotificaciones() = workerConsultas.enviarNotificaciones()
 
 }
