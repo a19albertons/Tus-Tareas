a8ca414d242e6828793c97256f676f7b8b92885f
a19albertons <a19albertons@iessanclemente.net>
2026-04-04 16:47:02 +0200

    Añadir comentarios en los enum de filtros

--- codigo/app/src/main/java/com/example/tustareas/filtros/OrdenarProyectoFin.kt
+++ codigo/app/src/main/java/com/example/tustareas/filtros/OrdenarProyectoFin.kt
@@ -1,5 +1,8 @@
 package com.example.tustareas.filtros
 
+/**
+ * Clase que representa la enumeracion de opciones de ordenación en filtro de proyecto en fin
+ */
 enum class OrdenarProyectoFin {
     FIN,
     FECHA_ASC,
--- codigo/app/src/main/java/com/example/tustareas/filtros/OrdenarProyectosInicio.kt
+++ codigo/app/src/main/java/com/example/tustareas/filtros/OrdenarProyectosInicio.kt
@@ -1,5 +1,8 @@
 package com.example.tustareas.filtros
 
+/**
+ * Clase que representa la enumeracion de opciones de ordenación en filtro de proyecto en inicio
+ */
 enum class OrdenarProyectosInicio {
     INICIO,
     FECHA_ASC,
