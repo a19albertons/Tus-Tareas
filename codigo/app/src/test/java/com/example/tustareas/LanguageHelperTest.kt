b93adab7a2a5ff88e0c41b753e4824e207549111
a19albertons <a19albertons@iessanclemente.net>
2026-04-13 20:10:43 +0200

    Actualizado con comentario el language helper

--- codigo/app/src/test/java/com/example/tustareas/LanguageHelperTest.kt
+++ codigo/app/src/test/java/com/example/tustareas/LanguageHelperTest.kt
@@ -4,38 +4,47 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.example.tustareas.util.LanguageHelper
 import org.junit.Test
 
+/**
+ * Clase que gestiona las pruebas unitarias del language helper
+ */
 class LanguageHelperTest {
 
+    // Test para comprobar la etiqueta del español
     @Test
     fun testEspanol() {
         val etiqueta = LanguageHelper.etiquetaIdioma("Español")
         assert(etiqueta == "es")
     }
 
+    // Test para comprobar la etqiueta del gallego
     @Test
     fun testGallego() {
         val etiqueta = LanguageHelper.etiquetaIdioma("Gallego")
         assert(etiqueta == "gl")
     }
 
+    // Test para comprobar la etiqueta del sistema
     @Test
     fun testSistema() {
         val etiqueta = LanguageHelper.etiquetaIdioma("Sistema")
         assert(etiqueta == "")
     }
 
+    // Test para comprobar la etiqueta del ingles
     @Test
     fun testIngles() {
         val etiqueta = LanguageHelper.etiquetaIdioma("Ingles")
         assert(etiqueta == "en")
     }
 
+    // Test para comprobar la etiqueta de cualquier idioma
     @Test
     fun testCualquierIdioma() {
         val etiqueta = LanguageHelper.etiquetaIdioma("idioma")
         assert(etiqueta == "")
     }
 
+    // Test de aplicar un idioma
     @Test
     fun aplicarIdioma() {
         LanguageHelper.aplicarIdioma("es")
