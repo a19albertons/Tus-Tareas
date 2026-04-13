b1713051c35143dfdd27476213000c7dee0402d6
a19albertons <a19albertons@iessanclemente.net>
2026-04-13 20:11:17 +0200

    Añadidas primeras preubas el date Helper

---
+++ codigo/app/src/test/java/com/example/tustareas/DateHelperTest.kt
@@ -0,0 +1,25 @@
+package com.example.tustareas
+
+import com.example.tustareas.util.DateHelper
+import org.junit.Test
+import java.util.Date
+
+/**
+ * Clase que gestiona las pruebas unitarias del date helper
+ */
+class DateHelperTest {
+
+    @Test
+    fun testTimestamp1() {
+        val timestamp = 1776031200000
+        val fecha = DateHelper.timestampToString(Date(timestamp))
+        println(fecha)
+        assert(fecha == "13/04/2026")
+    }
+
+    @Test
+    fun testTimestamp2() {
+        val fecha = DateHelper.timestampToString(null)
+        assert(fecha == "")
+    }
+}
