71e7913729f33acec03e30168769687c90897284
a19albertons <a19albertons@iessanclemente.net>
2026-04-30 00:04:22 +0200

    Creación de regla para corutinas

---
+++ codigo/app/src/androidTest/java/com/example/tustareas/helper/MainDispatcherRule.kt
@@ -0,0 +1,28 @@
+package com.example.tustareas.helper
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * Regla de JUnit para configurar una regla similar a la que se aplicaba a los observadores, pero
+ * en las corutinas en este escenario. Esto debería evitar race condition en los tests de integración y unitarios
+ * donde exista el riesgo. Siempre que se aplique
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
