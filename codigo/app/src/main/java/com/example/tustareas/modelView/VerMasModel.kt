aeff38504695860bfaecb41ad1bbffb9b94159bd
a19albertons <a19albertons@iessanclemente.net>
2026-04-30 00:31:40 +0200

    Añadida logica de negocio al modelo de ver más

--- codigo/app/src/main/java/com/example/tustareas/modelView/VerMasModel.kt
+++ codigo/app/src/main/java/com/example/tustareas/modelView/VerMasModel.kt
@@ -1,15 +1,25 @@
 package com.example.tustareas.modelView
 
+import android.widget.CheckBox
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.switchMap
+import com.example.tustareas.R
+import com.example.tustareas.modelos.Estado
 import com.example.tustareas.modelos.Tarea
 import com.example.tustareas.repository.TusTareasRepository
+import com.example.tustareas.util.DateHelper
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 /**
  * Clase que gestiona el submodelo del fragmento ver mas
  */
-class VerMasModel(private val repository: TusTareasRepository) {
+class VerMasModel(
+    private val repository: TusTareasRepository,
+    private val scope: CoroutineScope
+) {
     // Guarda la variable de texto del filtro
     private val textoVerMas = MutableLiveData("")
 
@@ -32,5 +42,37 @@ class VerMasModel(private val repository: TusTareasRepository) {
     }
 
     // Modificar tarea del adapter
-    suspend fun modificarTarea(tarea: Tarea) = repository.verMas.modificarTarea(tarea)
+    private suspend fun modificarTarea(tarea: Tarea) = repository.verMas.modificarTarea(tarea)
+
+    // Gestiona el click en el checkbox de la tarea. Se mueve la lógica de negocio al view Model
+    fun clickCheckbox(objectoActual: Tarea, checkBox: CheckBox) {
+        if (checkBox.isChecked) {
+            objectoActual.estado = Estado.Completada
+            scope.launch {
+                try {
+                    modificarTarea(objectoActual)
+                }
+                catch (_: Exception) {
+                    Snackbar.make(checkBox,checkBox.context.getString(R.string.error_modificar_checkbox), Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+        else {
+            // Control de la fecha EnTiempo o Retrasada
+            if (objectoActual.fechaLimite == null || objectoActual.fechaLimite!! >= DateHelper.fechaMediaNocheUTC()) {
+                objectoActual.estado = Estado.EnTiempo
+            }
+            else {
+                objectoActual.estado = Estado.Retrasada
+            }
+            scope.launch {
+                try {
+                    modificarTarea(objectoActual)
+                }
+                catch (_: Exception) {
+                    Snackbar.make(checkBox,checkBox.context.getString(R.string.error_modificar_checkbox), Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 }
