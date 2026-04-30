08c5c32bcaa399fe1588c49eb18e6793f5d67551
a19albertons <a19albertons@iessanclemente.net>
2026-04-30 19:11:46 +0200

    Movida lógica de negocio al modelo

--- codigo/app/src/main/java/com/example/tustareas/modelView/EstadisticasModel.kt
+++ codigo/app/src/main/java/com/example/tustareas/modelView/EstadisticasModel.kt
@@ -3,14 +3,23 @@ package com.example.tustareas.modelView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import com.example.tustareas.repository.TusTareasRepository
+import com.example.tustareas.util.DateHelper
 import com.github.mikephil.charting.data.BarEntry
+import java.util.Calendar
+import java.util.TimeZone
+import kotlin.div
+import kotlin.text.toFloat
+import kotlin.times
 
 /**
  * Clase que gestiona  el submodelo de estadisticas
  */
 class EstadisticasModel(private val repository: TusTareasRepository) {
     // Generación tercera grafica de estadisticas
-    fun obtenerDatosGrafico(timestampDiasSemana: LongArray): LiveData<List<BarEntry>> {
+    fun obtenerDatosGrafico(): LiveData<List<BarEntry>> {
+        // Obtener timestamp de la semana
+        val timestampDiasSemana = obtenerTimestampsDiasSemana()
+
         // variable base (mediadiador, completa, no completas)
         val resultado = MediatorLiveData<List<BarEntry>>()
         val completas = timestampDiasSemana.map { it -> repository.estadisticas.tareasCompletadasPorDia(it)  }
@@ -44,26 +53,57 @@ class EstadisticasModel(private val repository: TusTareasRepository) {
 
 
     // Generacion rueda (primer grafico)
-    fun obtenerRueda(fechaInicio: Long, fechaFin: Long): LiveData<Pair<Long, Long>> {
+    fun obtenerRueda(): LiveData<Float> {
+        // Obtener timestamp de la semana
+        val timestampDiasSemana = obtenerTimestampsDiasSemana()
+
+        // Fechas de inicio y fin de la semana
+        val fechaInicio = timestampDiasSemana.first()
+        val fechaFin = timestampDiasSemana.last()
+
+
         // variable base
-        val resultado = MediatorLiveData<Pair<Long, Long>>()
+        val resultado = MediatorLiveData<Float>()
         val completas = repository.estadisticas.obtenerCantidadTareasCompletasEntre2Fechas(fechaInicio, fechaFin)
         val noCompletas = repository.estadisticas.obtenerCantidadTareasPendientesEntre2Fechas(fechaInicio, fechaFin)
 
         // actualizador
         val valores = {
-            val c = completas.value ?: 0
-            val nc = noCompletas.value ?: 0
-            resultado.value = Pair(c, nc)
+            val c = completas.value ?: 0L
+            val nc = noCompletas.value ?: 0L
+            val total = c + nc
+            if (total == 0L) {
+                resultado.value = 0f
+            } else {
+                resultado.value = c.toFloat() / total.toFloat() * 100
+            }
         }
 
         // Observar
         resultado.addSource(completas) { valores() }
         resultado.addSource(noCompletas) { valores() }
 
-        // resultado
         return resultado
 
-
     }
+
+    // Calculo de los timestamps de cada dia de la semana actual (lunes a domingo) con 00:00:00 en UTC y devuelve el array con los valores
+    private fun obtenerTimestampsDiasSemana(): LongArray {
+        // Configurar el calendar para que se situe en el lunes de la actual semana con 00:00:00 en UTC
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        calendar.time = DateHelper.fechaMediaNocheUTC()
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        val devolver = LongArray(7) {
+            val timestampDia = calendar.timeInMillis
+            calendar.add(Calendar.DAY_OF_MONTH, 1)
+            timestampDia
+        }
+        return devolver
+    }
+    
 }
