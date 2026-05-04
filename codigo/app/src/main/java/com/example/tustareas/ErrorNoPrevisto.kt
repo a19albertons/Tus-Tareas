cbd81a5310c1052292d7c0f19187c78a6468c98d
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-05-04 20:22:00 +0200

    Nuevas clases para gestión de errores no previstos

---
+++ codigo/app/src/main/java/com/example/tustareas/ErrorNoPrevisto.kt
@@ -0,0 +1,29 @@
+package com.example.tustareas
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import kotlin.system.exitProcess
+
+/**
+ * Actividad independiente que muestra un error irreversible del fragmento previo o no controlado por el código.
+ *
+ * @author Alberto Noceda <a19albertons@iessanclemente.net>
+ */
+class ErrorNoPrevisto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error_no_previsto)
+
+        // Mostrar error en pantalla
+        val mensaje = intent.getStringExtra("error") ?: "Error desconocido"
+        findViewById<TextView>(R.id.excepcion).text = mensaje
+
+        // Boton para cerrar la aplicación
+        findViewById<MaterialButton>(R.id.cerrar).setOnClickListener {
+            finishAffinity()
+            exitProcess(0)
+        }
+    }
+}
---
+++ codigo/app/src/main/java/com/example/tustareas/TusTareasApp.kt
@@ -0,0 +1,49 @@
+package com.example.tustareas
+
+import android.app.Application
+import android.content.Intent
+import android.util.Log
+import kotlin.jvm.java
+import kotlin.system.exitProcess
+
+/**
+ * Clase Application personalizada para manejar errores no controlados a nivel global.
+ *
+ * @author Alberto Noceda <a19albertons@iessanclemente.net>
+ */
+class TusTareasApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Obtiene un manejador de errorres de tipo por defecto
+        val capturadorErrores = Thread.getDefaultUncaughtExceptionHandler()
+
+        // Controla la captura de errores no controlados
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            // Si el packagename terminar por error invocamos este activity
+            if (packageName.endsWith(":error_process")) {
+                capturadorErrores?.uncaughtException(thread, throwable)
+                return@setDefaultUncaughtExceptionHandler
+            }
+
+
+            // Obtener el stack trace completo
+            val stackTrace = throwable.stackTraceToString()
+
+            // Genera un log para logcat con el error no previsto
+            Log.e("Error no previsto", stackTrace, throwable)
+
+            // Lanzamos el activity que muestra el error al usuario.
+            val intent = Intent(this, ErrorNoPrevisto::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                putExtra("error", getString(R.string.error_no_previsto)+stackTrace)
+            }
+            // Lanzamos la actividad
+            startActivity(intent)
+
+            // MAtamos el activity previo y lo cerramos para evitar fugas de memoria y potenciales errores
+            android.os.Process.killProcess(android.os.Process.myPid())
+            exitProcess(10)
+        }
+    }
+}
