39de351cde616d412273b0d010102db9a497d1da
a19albertons <a19albertons@iessanclemente.net>
2026-05-10 17:03:00 +0200

    Actualizar el worker para que use Hilt

--- codigo/app/src/main/java/com/example/tustareas/workers/ActualizarEstadoWorker.kt
+++ codigo/app/src/main/java/com/example/tustareas/workers/ActualizarEstadoWorker.kt
@@ -1,18 +1,25 @@
 package com.example.tustareas.workers
 
 import android.content.Context
-import android.util.Log
+import androidx.hilt.work.HiltWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.tustareas.db.TusTareasDatabase
 import com.example.tustareas.repository.WorkerRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
 /**
  * Clase que gestiona el paso de tareas que exceden su tiempo limite y aún no estan completadas de acuerdo al usuario
  *
  * @author Alberto Noceda <a19albertons@iessanclemente.net>
  */
-class ActualizarEstadoWorker(appContext: Context, workerParams: WorkerParameters): Worker(appContext, workerParams) {
+@HiltWorker
+class ActualizarEstadoWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val repositorio : WorkerRepository
+): Worker(appContext, workerParams) {
     // Sobreescribe el metodo que indica el trabajo
     /**
      * Realiza el trabajo de revisar las tareas y actualizar su estado si es necesario
@@ -40,8 +47,6 @@ class ActualizarEstadoWorker(appContext: Context, workerParams: WorkerParameters
      * @author Alberto Noceda <a19albertons@iessanclemente.net>
      */
     private fun revisarEstados() {
-        val database = TusTareasDatabase.getDatabase(applicationContext)
-        val repositorio = WorkerRepository(database)
         repositorio.actualizarEstado()
     }
 }
