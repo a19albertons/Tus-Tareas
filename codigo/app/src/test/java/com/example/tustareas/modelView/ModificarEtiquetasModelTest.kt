48f77ed192e079457c3af3228228e0a0e9d8c638
a19albertons <a19albertons@iessanclemente.net>
2026-05-13 18:46:57 +0200

    Revisión test unitarios

--- codigo/app/src/test/java/com/example/tustareas/modelView/ModificarEtiquetasModelTest.kt
+++ codigo/app/src/test/java/com/example/tustareas/modelView/ModificarEtiquetasModelTest.kt
@@ -2,6 +2,8 @@ package com.example.tustareas.modelView
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.tustareas.R
+import com.example.tustareas.helper.MainDispatcherRule
 import com.example.tustareas.modelos.Etiqueta
 import com.example.tustareas.repository.ModificarEtiquetasRepository
 import kotlinx.coroutines.test.runTest
@@ -19,37 +21,127 @@ class ModificarEtiquetasModelTest {
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val mainRule = MainDispatcherRule()
+
     // Definición repositorio
     val modificarEtiquetasRepository = Mockito.mock(ModificarEtiquetasRepository::class.java)
 
     // Definición modelo
-    val modificarEtiquetasModel = ModificarEtiquetasModel(Application(), modificarEtiquetasRepository)
+    val modificarEtiquetasModel =
+        ModificarEtiquetasModel(Application(), modificarEtiquetasRepository)
+
 
     @Test
-    fun insertarEtiqueta() = runTest {
+    fun definirEtiquetayObservarEtiqueta() {
         // Definición etiqueta de prueba
         val etiqueta = Etiqueta(1, "Etiqueta 1", "Descripción de la etiqueta 1")
 
-        // Definición respuestas
-        Mockito.`when`(modificarEtiquetasRepository.insertarEtiqueta(etiqueta))
-            .thenReturn(Unit)
+        // Definir una etiqueta
+        modificarEtiquetasModel.definirEtiqueta(etiqueta)
 
         // Comprobación del resultado
-        modificarEtiquetasModel.insertarEtiqueta(etiqueta)
-        Mockito.verify(modificarEtiquetasRepository).insertarEtiqueta(etiqueta)
+        assert(modificarEtiquetasModel.observarEtiqueta().value == etiqueta)
     }
 
     @Test
-    fun modificarEtiqueta() = runTest {
+    fun tituloDialogoNueva() {
+        // Definición etiqueta de prueba
+        val etiqueta = Etiqueta(0, "Etiqueta 1", "Descripción de la etiqueta 1")
+
+        // Definir una etiqueta
+        modificarEtiquetasModel.definirEtiqueta(etiqueta)
+
+        // Comprobación del resultado
+        assert(modificarEtiquetasModel.tituloDialogo() == R.string.confirmar_guardar_etiqueta)
+    }
+
+    @Test
+    fun tituloDialogoExistente() {
         // Definición etiqueta de prueba
         val etiqueta = Etiqueta(1, "Etiqueta 1", "Descripción de la etiqueta 1")
 
-        // Definición respuestas
-        Mockito.`when`(modificarEtiquetasRepository.modificarEtiqueta(etiqueta))
-            .thenReturn(Unit)
+        // Definir una etiqueta
+        modificarEtiquetasModel.definirEtiqueta(etiqueta)
 
         // Comprobación del resultado
-        modificarEtiquetasModel.modificarEtiqueta(etiqueta)
+        assert(modificarEtiquetasModel.tituloDialogo() == R.string.confirmar_modificar_etiqueta)
+    }
+
+    // Guardado de nuevas etiquetas
+    @Test
+    fun guardarEtiqueta() = runTest {
+        // Definición etiqueta de prueba
+        val etiqueta = Etiqueta(0, "Etiqueta 1", "Descripción de la etiqueta 1")
+
+        // Definir una etiqueta
+        modificarEtiquetasModel.definirEtiqueta(etiqueta)
+
+        // Guardar la etiqueta
+        modificarEtiquetasModel.guardarYModificarEtiqueta("Etiqueta 1", "Descripción de la etiqueta 1")
+
+        // Comprobación del resultado
+        Mockito.verify(modificarEtiquetasRepository).insertarEtiqueta(etiqueta)
+
+        // Valor del resultado
+        assert(modificarEtiquetasModel.observarResultado().value == true)
+
+    }
+
+    // Guardado de nuevas etiquetas no valida
+    @Test
+    fun guardarEtiquetaInvalida() = runTest {
+        // Definición etiqueta de prueba
+        val etiqueta = Etiqueta(0, "Etiqueta 1", "Descripción de la etiqueta 1")
+
+        // Definir una etiqueta
+        modificarEtiquetasModel.definirEtiqueta(etiqueta)
+
+        // Guardar la etiqueta
+        modificarEtiquetasModel.guardarYModificarEtiqueta(" ", "Descripción de la etiqueta 1")
+
+        // Comprobación del resultado
+        assert(modificarEtiquetasModel.observarMensajeError().value == R.string.error_guardar_etiqueta)
+
+        // Valor del resultado
+        assert(modificarEtiquetasModel.observarResultado().value == false)
+    }
+
+    // Guardado de nuevas etiquetas
+    @Test
+    fun guardarEtiquetaExistente() = runTest {
+        // Definición etiqueta de prueba
+        val etiqueta = Etiqueta(1, "Etiqueta 1", "Descripción de la etiqueta 1")
+
+        // Definir una etiqueta
+        modificarEtiquetasModel.definirEtiqueta(etiqueta)
+
+        // Guardar la etiqueta
+        modificarEtiquetasModel.guardarYModificarEtiqueta("Etiqueta 1", "Descripción de la etiqueta 1")
+
+        // Comprobación del resultado
         Mockito.verify(modificarEtiquetasRepository).modificarEtiqueta(etiqueta)
+
+        // Valor del resultado
+        assert(modificarEtiquetasModel.observarResultado().value == true)
+    }
+
+    // Guardado de nuevas etiquetas no valida
+    @Test
+    fun guardarEtiquetaExistenteInvalida() = runTest {
+        // Definición etiqueta de prueba
+        val etiqueta = Etiqueta(1, "Etiqueta 1", "Descripción de la etiqueta 1")
+
+        // Definir una etiqueta
+        modificarEtiquetasModel.definirEtiqueta(etiqueta)
+
+        // Guardar la etiqueta
+        modificarEtiquetasModel.guardarYModificarEtiqueta(" ", "Descripción de la etiqueta 1")
+
+        // Comprobación del resultado
+        assert(modificarEtiquetasModel.observarMensajeError().value == R.string.error_modificar_etiqueta)
+
+        // Valor del resultado
+        assert(modificarEtiquetasModel.observarResultado().value == false)
     }
 }
