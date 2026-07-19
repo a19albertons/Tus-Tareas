6e2c579f0e675ebdf0ad4592abe6cac8d8eaa899
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-07-19 09:58:37 +0200

    Creamos un subdirectorio con elementos de debug

---
+++ codigo/app/src/debug/java/com/example/tustareas/HiltTestActivity.kt
@@ -0,0 +1,10 @@
+package com.example.tustareas
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Personaliza el scenario fragment para soportar hilt
+ */
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
