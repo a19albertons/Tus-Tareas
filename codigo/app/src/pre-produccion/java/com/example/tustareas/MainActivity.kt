3a3ae06253214f2c38eb8a7399c7b14d1b520f45
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-07-19 10:33:22 +0200

    Correción banda gris en ciertas pantallas

--- codigo/app/src/pre-produccion/java/com/example/tustareas/MainActivity.kt
+++ codigo/app/src/pre-produccion/java/com/example/tustareas/MainActivity.kt
@@ -78,11 +78,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         // Obtencion nav controller
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
